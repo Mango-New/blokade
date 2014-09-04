845f83ba884440cca448521cc2c141eda7b3ba44
Mark D Holmberg <mark.d.holmberg@gmail.com>
2014-09-04 10:35:22 -0600

    Changes ability concern to check if we respond_to? certain methods. #3.

--- lib/blokade/acts_as_ability.rb
+++ lib/blokade/acts_as_ability.rb
@@ -7,34 +7,43 @@ module Blokade
         # This is the dynamic permission functionality
 
         # BACK-END PERMISSIONS
-        user.permissions.backend.each do |permission|
-          can permission.action.to_sym, permission.subject_class.constantize
-        end
-
-        # FRONT-END PERMISSIONS
-        blokade_id_column = "#{Blokade.blokadable_klass.model_name.element}_id"
-
-        # Unrestricted Frontend Permissions
-        user.permissions.frontend.not_symbolic.unrestricted.each do |permission|
-          # Check a users permissions
-          if permission.subject_class.constantize.column_names.include?(blokade_id_column)
-            # Does this model have a blokadable_id column in it...
-            can permission.action.to_sym, permission.subject_class.constantize, blokade_id_column.to_sym => user.send(blokade_id_column)
-          else
-            # ... or is it the blokadable itself?
-            can permission.action.to_sym, permission.subject_class.constantize, :id => user.send(blokade_id_column)
+        if user.respond_to?(:permissions)
+          if user.permissions.respond_to?(:backend)
+            user.permissions.backend.each do |permission|
+              can permission.action.to_sym, permission.subject_class.constantize
+            end
           end
         end
 
-        # Restricted Frontend Permissions
-        user.permissions.frontend.not_symbolic.restricted.each do |permission|
-          # Check a users permissions
-          if permission.subject_class.constantize.column_names.include?(blokade_id_column)
-            # Does this model have a blokadable_id column in it...
-            can permission.action.to_sym, permission.subject_class.constantize, blokade_id_column.to_sym => user.send(blokade_id_column), permission.subject_class.constantize.my_frontend_restrictions.to_sym => user.id
-          else
-            # ... or is it the blokadable itself?
-            can permission.action.to_sym, permission.subject_class.constantize, :id => user.send(blokade_id_column)#, permission.subject_class.constantize.my_restrictions.to_sym => user.id
+        # FRONT-END PERMISSIONS
+        if user.respond_to?(:permissions)
+          if user.permissions.respond_to?(:frontend)
+            # TODO: make this work with multiple companies
+            blokade_id_column = "#{Blokade.blokadable_klass.model_name.element}_id"
+
+            # Unrestricted Frontend Permissions
+            user.permissions.frontend.not_symbolic.unrestricted.each do |permission|
+              # Check a users permissions
+              if permission.subject_class.constantize.column_names.include?(blokade_id_column)
+                # Does this model have a blokadable_id column in it...
+                can permission.action.to_sym, permission.subject_class.constantize, blokade_id_column.to_sym => user.send(blokade_id_column)
+              else
+                # ... or is it the blokadable itself?
+                can permission.action.to_sym, permission.subject_class.constantize, :id => user.send(blokade_id_column)
+              end
+            end
+
+            # Restricted Frontend Permissions
+            user.permissions.frontend.not_symbolic.restricted.each do |permission|
+              # Check a users permissions
+              if permission.subject_class.constantize.column_names.include?(blokade_id_column)
+                # Does this model have a blokadable_id column in it...
+                can permission.action.to_sym, permission.subject_class.constantize, blokade_id_column.to_sym => user.send(blokade_id_column), permission.subject_class.constantize.my_frontend_restrictions.to_sym => user.id
+              else
+                # ... or is it the blokadable itself?
+                can permission.action.to_sym, permission.subject_class.constantize, :id => user.send(blokade_id_column)#, permission.subject_class.constantize.my_restrictions.to_sym => user.id
+              end
+            end
           end
         end
 
