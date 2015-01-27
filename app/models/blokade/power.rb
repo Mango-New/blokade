18f575e43b93fb5084a4ce419e80b07efef6d080
Mark D Holmberg <mark.d.holmberg@gmail.com>
2015-01-27 08:26:56 -0700

    Patches Power user id uniqueness validation.

--- app/models/blokade/power.rb
+++ app/models/blokade/power.rb
@@ -5,8 +5,9 @@ module Blokade
 
     # Note: We don't allow the user to be assigned the same power twice (it wouldn't make sense!)
     # Note: We don't require the presence of user_id because if we did, then we can do `build` properly
+    # Update: Technically this *could* allow a user_id of `nil`, but that's the programmer's concern.
     validates :user_id,
-      uniqueness: { scope: [:role_id] }
+      uniqueness: { scope: [:role_id] }, if: Proc.new { |k| k.user_id.present? }
     validates :role_id, presence: true
   end
 end
